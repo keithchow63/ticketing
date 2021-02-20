8a85d8f90246f39ce17af473ba3009d6987c3490
Keith Chow <keithchow63@gmail.com>
2021-02-20 21:30:56 +0800

    fix nginx bug

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // on the server
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-app-keith.xyz',
       headers: req.headers
     })
   } else {
